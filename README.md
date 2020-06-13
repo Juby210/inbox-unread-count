cb968a6b0dc41afca1898f045a7e3e9e3198720f
Juby210 <juby210@vp.pl>
2020-06-13 17:45:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# inbox-unread-count
+Powercord plugin. Brings back unread counter to inbox (enable inbox in experiments if you don't have it).
+
+![preview](https://i.imgur.com/HhZJvfq.png)
---
+++ index.js
@@ -0,0 +1,48 @@
+const { Plugin } = require('powercord/entities')
+const { Text } = require('powercord/components')
+const { getModule, React } = require('powercord/webpack')
+const { inject, uninject } = require('powercord/injector')
+
+module.exports = class InboxUnreadCount extends Plugin {
+    async startPlugin() {
+        const { dateFormat, isSameDay } = await getModule(['dateFormat'])
+        const AnimatedUnreadChannelMessages = await getModule(m => m.type && m.type.displayName == 'AnimatedUnreadChannelMessages')
+
+        inject('inbox-unread-count', AnimatedUnreadChannelMessages, 'type', (args, res) => {
+            console.log(args, res)
+            if (!res.props.children || !res.props.children.type || !res.props.children.type.type) return res
+
+            const { type } = res.props.children.type
+            if (type._inbox_count) return res
+            res.props.children.type.type = (...a) => {
+                const r = type(...a)
+                if (!r) return r
+                console.log(a, r)
+
+                const { messages } = a[0].channel
+                console.log(messages)
+                if (!messages.length) return r
+                const since = dateFormat(messages[0].timestamp, isSameDay(messages[0].timestamp, { toDate: () => new Date }) ? 'LT' : 'LLL')
+                r.props.children.splice(1, 0,
+                    React.createElement(Text, {
+                            color: Text.Colors.HEADER_SECONDARY,
+                            style: { margin: '0 7px 7px' }
+                        },
+                        `${messages.length > 25 ? '25+' : messages.length} unreads since ${since}`
+                    )
+                )
+
+                return r
+            }
+            res.props.children.type.type.displayName = type.displayName
+            res.props.children.type.type._inbox_count = true
+
+            return res
+        })
+        AnimatedUnreadChannelMessages.type.displayName = 'AnimatedUnreadChannelMessages'
+    }
+
+    pluginWillUnload() {
+        uninject('inbox-unread-count')
+    }
+}
